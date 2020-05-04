b9daf7f779c328fc589befea845963b0a05220d2
Thulani Mtetwa <Thulani.Mtetwa@standardbank.co.za>
2020-05-04 15:20:10 +0200

    Configure UI

---
+++ ProjectGoto/Controllers/BullsEyeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BullsEyeViewController.swift
+//  ProjectGoto
+//
+//  Created by Thulani Mtetwa on 2020/05/04.
+//  Copyright © 2020 Thulani Mtetwa. All rights reserved.
+//
+
+import UIKit
+
+class BullsEyeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
