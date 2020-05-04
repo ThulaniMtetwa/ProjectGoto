68857d2dd5c109ee935fcc20b801a289ef560bc6
Thulani Mtetwa <Thulani.Mtetwa@standardbank.co.za>
2020-05-04 15:44:42 +0200

    Add logic for bulls eyes

--- ProjectGoto/Controllers/BullsEyeViewController.swift
+++ ProjectGoto/Controllers/BullsEyeViewController.swift
@@ -9,22 +9,50 @@
 import UIKit
 
 class BullsEyeViewController: UIViewController {
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let roundedValue = self.slider.value.rounded()
+        self.currentValue = Int(roundedValue)
+        self.startNewRound()
+    }
+ 
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        let roundedValue = self.slider.value.rounded()
+        self.currentValue = Int(roundedValue)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func showAlert() {
+        
+        let message = "The value of the slider is now: \(self.currentValue)" +
+        "\nThe target value is: \(self.targetValue)"
+        
+        let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        self.startNewRound()
     }
-    */
-
+    
+    func startNewRound() {
+        self.targetValue = Int.random(in: 1...100)
+        self.currentValue = 50
+        self.slider.value = Float(self.currentValue)
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        self.targetLabel.text = String(self.targetValue)
+    }
 }
